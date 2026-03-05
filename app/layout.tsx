a1cb2b36d571f4509f0f2c0ffbd82abec1c2d5c0
ITALY\gusfonseca <gustavo.valle@engdb.com.br>
2026-03-05 09:33:01 -0300

    Adicionando anuncio no layout principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import Script from "next/script"
 import "./globals.css"
 
 export const metadata = {
@@ -15,6 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5817280149271380"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+
       <body>
         <Header />
         <main style={{ maxWidth: 900, margin: "0 auto", padding: 20 }}>
